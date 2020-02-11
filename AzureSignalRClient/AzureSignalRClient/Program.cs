e307ba8058ff167dc8f65c7014002e4671c9b78c
shuheydev <shuhey.dev@gmail.com>
2020-02-11 10:42:38 +0900

    Initial commit

---
+++ AzureSignalRClient/AzureSignalRClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureSignalRClient
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            HubConnection connection;
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/chat")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.On<string, string>("broadcastMessage", (string name, string message) =>
+            {
+                if (string.IsNullOrEmpty(message))
+                {
+                    return;
+                }
+
+                Console.WriteLine($"{name}: {message}");
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                Console.WriteLine("接続できました");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("接続できませんでした");
+            }
+
+            string input;
+            while (true)
+            {
+                if ((input = Console.ReadLine()) == "exit")
+                    break;
+
+                await connection.InvokeAsync("BroadcastMessage", "console", input);
+            }
+        }
+    }
+}
